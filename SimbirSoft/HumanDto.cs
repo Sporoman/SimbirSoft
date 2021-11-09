7cca492783d91308bc96ec6aac55bb3b6baf5954
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-09 12:39:40 +0400

    first commit

---
+++ SimbirSoft/HumanDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SimbirSoft
+{
+    /// <summary>
+    /// 1.2.1 - Класс представления пользователя
+    /// </summary>
+    public class HumanDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Birthday { get; set; }
+    }
+}
