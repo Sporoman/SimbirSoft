f16fdc845abef1eefc8d249d5cd62672c1aff648
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-14 17:12:33 +0400

    + swagger

--- SimbirSoft/Startup.cs
+++ SimbirSoft/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace SimbirSoft
 {
@@ -11,6 +12,10 @@ namespace SimbirSoft
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Simbirsoft", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -18,6 +23,8 @@ namespace SimbirSoft
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "Simbirsoft v1"));
             }
 
             app.UseRouting();
