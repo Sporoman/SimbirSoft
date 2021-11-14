f6d1ffdf7fcd18a2fa53cf27c5acd576a0634297
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-14 16:39:45 +0400

    + методы get set

--- SimbirSoft/TestData.cs
+++ SimbirSoft/TestData.cs
@@ -7,18 +7,21 @@ namespace SimbirSoft
     /// </summary>
     public static class TestData
     {
+        static public int IDHuman = 3;
+        static public int IDBook  = 3;
+
         static private List<HumanDto> _humansList = new List<HumanDto>()
         {
-            new HumanDto{ Surname = "Ульянов",  Name = "Владимир", Patronymic = "Ильич",    Birthday = "22.04.1870"},
-            new HumanDto{ Surname = "Воронин",  Name = "Николай",  Patronymic = "Петрович", Birthday = "12.05.1963"},
-            new HumanDto{ Surname = "Максимов", Name = "Андрей",   Patronymic = "Иванович", Birthday = "21.10.2000"}
+            new HumanDto{ ID = 1, Surname = "Ульянов",  Name = "Владимир", Patronymic = "Ильич",    Birthday = "22.04.1870", Writer = true  },
+            new HumanDto{ ID = 2, Surname = "Воронин",  Name = "Николай",  Patronymic = "Петрович", Birthday = "12.05.1963", Writer = false },
+            new HumanDto{ ID = 3, Surname = "Максимов", Name = "Андрей",   Patronymic = "Иванович", Birthday = "21.10.2000", Writer = true  }
         };
 
         static private List<BookDto> _booksList = new List<BookDto>()
         {
-            new BookDto{ Title = "Синее пламя",      Genre = "Фэнтези", Author = "Воронин Дмитрий Анатольевич"},
-            new BookDto{ Title = "Хижина дяди Тома", Genre = "Роман",   Author = "Гарриет Бичер-Стоу"},
-            new BookDto{ Title = "Плоды земли",      Genre = "Роман",   Author = "Кнут Гамсун"}
+            new BookDto{ ID = 1, Title = "Синее пламя",      Genre = "Фэнтези", Author = "Воронин Дмитрий Анатольевич"},
+            new BookDto{ ID = 2, Title = "Хижина дяди Тома", Genre = "Роман",   Author = "Гарриет Бичер-Стоу"},
+            new BookDto{ ID = 3, Title = "Плоды земли",      Genre = "Роман",   Author = "Кнут Гамсун"}
         };
 
         public static List<HumanDto> GetHumansList()
@@ -28,6 +31,10 @@ namespace SimbirSoft
 
         public static void AddHumanToList(HumanDto unit)
         {
+            // Присваиваем ID записи и увеличиваем счётчик
+            unit.ID = IDHuman;
+            ++IDHuman;
+
             _humansList.Add(unit);
         }
         public static List<BookDto> GetBooksList()
@@ -37,6 +44,10 @@ namespace SimbirSoft
 
         public static void AddBookToList(BookDto unit)
         {
+            // Присваиваем ID записи и увеличиваем счётчик
+            unit.ID = IDBook;
+            ++IDBook;
+
             _booksList.Add(unit);
         }
     }
