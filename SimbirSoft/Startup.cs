3ed379fe317e6c230413886020279545f0d744e6
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-14 14:43:27 +0400

    Добавление MVC

--- SimbirSoft/Startup.cs
+++ SimbirSoft/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Collections.Generic;
 
 namespace SimbirSoft
 {
@@ -11,10 +10,16 @@ namespace SimbirSoft
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints => 
