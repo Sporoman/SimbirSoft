b837f9bc287f51e3fd4a9d39e9df0d3c12a61217
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-14 16:40:04 +0400

    + id

--- SimbirSoft/Models/BookDto.cs
+++ SimbirSoft/Models/BookDto.cs
@@ -5,6 +5,7 @@
     /// </summary>
     public class BookDto
     {
+        public int ID { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string Genre { get; set; }
--- SimbirSoft/Models/HumanDto.cs
+++ SimbirSoft/Models/HumanDto.cs
@@ -5,9 +5,11 @@
     /// </summary>
     public class HumanDto
     {
+        public int ID { get; set; }
         public string Surname { get; set; }
         public string Name { get; set; }
         public string Patronymic { get; set; }
         public string Birthday { get; set; }
+        public bool Writer { get; set; }
     }
 }
