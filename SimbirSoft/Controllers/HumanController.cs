11f6c7eebc28f72ed72dc05d7465d91866d65d07
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-14 16:40:30 +0400

    Добавление необходимых методов get

--- SimbirSoft/Controllers/HumanController.cs
+++ SimbirSoft/Controllers/HumanController.cs
@@ -27,15 +27,53 @@ namespace SimbirSoft.Controllers
         /// 3.1.2 Get для всех людей, которые пишут книги
         /// </summary>
         /// <returns></returns>
-        //[HttpGet]
-        //public IEnumerable<HumanDto> Get
+        [HttpGet("writers")]
+        public IEnumerable<HumanDto> GetWriters()
+        {
+            return TestData.GetHumansList().Where(unit => unit.Writer == true);
+        }
 
         /// <summary>
         /// 3.1.3 Get для всех людей, в имени, фамилии или отчестве которых содержится поисковая фраза (query).
         /// </summary>
         /// <returns></returns>
-        //[HttpGet]
-        //public IEnumerable<HumanDto> Get
+        [HttpGet("search")]
+        public IEnumerable<HumanDto> GetHumanByString(string queryStr)
+        {
+            // queryStr на пустую строку
+            if (queryStr == null)
+                return GetHumans();
 
+            // Переводим строку в нижний регистр
+            queryStr = queryStr.ToLower();
+
+            // Объявляем новый список для отбора необходимых людей
+            var list = new List<HumanDto>();
+
+            foreach (var unit in TestData.GetHumansList())
+            {
+                // Складываем части полного имени в одну строку
+                string FullName = unit.Name + unit.Patronymic + unit.Surname;
+
+                // Переводим строку в нижний регистр
+                FullName = FullName.ToLower();
+
+                if (FullName.Contains(queryStr))
+                    list.Add(unit);
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// 3.2 POST добавляющий нового человека.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public void AddHuman([FromBody] HumanDto unit)
+        {
+            TestData.AddHumanToList(new HumanDto { Surname = unit.Surname, Name = unit.Name, 
+                Patronymic = unit.Patronymic, Birthday = unit.Birthday, Writer = unit.Writer });
+        }
     }
 }
