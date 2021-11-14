589a3fc8afb5bdc5b6f96cedfcdf628db5fbf1e2
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-14 17:37:25 +0400

    Замена Author на AuthorID

--- SimbirSoft/Models/BookDto.cs
+++ SimbirSoft/Models/BookDto.cs
@@ -7,7 +7,7 @@
     {
         public int ID { get; set; }
         public string Title { get; set; }
-        public string Author { get; set; }
+        public int AuthorID { get; set; }
         public string Genre { get; set; }
     }
 }
