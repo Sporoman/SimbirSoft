e44000ca8e35b7a0b5ebbda01a34360a1c4e4f12
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-14 20:30:20 +0400

    + Модель LibraryCard

---
+++ SimbirSoft/Models/LibraryCardDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimbirSoft.Models
+{
+    /// <summary>
+    /// Часть 2.1; 1 - Новая сущность LibraryCard
+    /// </summary>
+    public class LibraryCardDto
+    {
+        public int ID { get; set; }
+        public HumanDto Human { get; set; }
+        public BookDto Book { get; set; }
+        public DateTimeOffset DateOfGive { get; set; }
+    }
+}
