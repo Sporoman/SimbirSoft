220f78f51af60c2549bf667ddbcc0b0fefe1e581
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-14 14:44:38 +0400

    Добавление контроллера Human

--- SimbirSoft/Controllers/HumanController.cs
+++ SimbirSoft/Controllers/HumanController.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+/// <summary>
+/// 3 - Контроллер, отвечающий за человека
+/// </summary>
 namespace SimbirSoft.Controllers
 {
-    public class HumanController
+    [ApiController]
+    [Route("[controller]")]
+    public class HumanController : ControllerBase
     {
+        /// <summary>
+        /// 3.1.1 Get для всех людей
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<HumanDto> GetHumans()
+        {
+            return TestData.GetHumansList();
+        }
+
+        /// <summary>
+        /// 3.1.2 Get для всех людей, которые пишут книги
+        /// </summary>
+        /// <returns></returns>
+        //[HttpGet]
+        //public IEnumerable<HumanDto> Get
+
+        /// <summary>
+        /// 3.1.3 Get для всех людей, в имени, фамилии или отчестве которых содержится поисковая фраза (query).
+        /// </summary>
+        /// <returns></returns>
+        //[HttpGet]
+        //public IEnumerable<HumanDto> Get
 
     }
 }
