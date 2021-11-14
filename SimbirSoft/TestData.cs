fdc2507da3a7201fca5e905a85d3316cc28ff843
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-14 14:44:59 +0400

    Изменение работы со стартовыми данными

--- SimbirSoft/TestData.cs
+++ SimbirSoft/TestData.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace SimbirSoft
 {
@@ -10,18 +7,37 @@ namespace SimbirSoft
     /// </summary>
     public static class TestData
     {
-        static public readonly List<HumanDto> _humanlist = new List<HumanDto>()
+        static private List<HumanDto> _humansList = new List<HumanDto>()
         {
             new HumanDto{ Surname = "Ульянов",  Name = "Владимир", Patronymic = "Ильич",    Birthday = "22.04.1870"},
             new HumanDto{ Surname = "Воронин",  Name = "Николай",  Patronymic = "Петрович", Birthday = "12.05.1963"},
             new HumanDto{ Surname = "Максимов", Name = "Андрей",   Patronymic = "Иванович", Birthday = "21.10.2000"}
         };
 
-        static public readonly List<BookDto> _booklist = new List<BookDto>()
+        static private List<BookDto> _booksList = new List<BookDto>()
         {
             new BookDto{ Title = "Синее пламя",      Genre = "Фэнтези", Author = "Воронин Дмитрий Анатольевич"},
             new BookDto{ Title = "Хижина дяди Тома", Genre = "Роман",   Author = "Гарриет Бичер-Стоу"},
             new BookDto{ Title = "Плоды земли",      Genre = "Роман",   Author = "Кнут Гамсун"}
         };
+
+        public static List<HumanDto> GetHumansList()
+        {
+            return _humansList;
+        }
+
+        public static void AddHumanToList(HumanDto unit)
+        {
+            _humansList.Add(unit);
+        }
+        public static List<BookDto> GetBooksList()
+        {
+            return _booksList;
+        }
+
+        public static void AddBookToList(BookDto unit)
+        {
+            _booksList.Add(unit);
+        }
     }
 }
