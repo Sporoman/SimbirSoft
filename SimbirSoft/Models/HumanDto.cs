2a61960094e11bfa59821a38be657434bfadfb74
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-15 11:16:04 +0400

    + Валидация

--- SimbirSoft/Models/HumanDto.cs
+++ SimbirSoft/Models/HumanDto.cs
@@ -1,14 +1,21 @@
-﻿namespace SimbirSoft.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimbirSoft.Models
 {
     /// <summary>
     /// 2.2.1 - Класс представления пользователя
+    /// Часть 2.2; 1 - Добавлена валидация
     /// </summary>
     public class HumanDto
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Не указана Фамилия")]
         public string Surname { get; set; }
+        [Required(ErrorMessage = "Не указано Имя")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Не указано Отчество")]
         public string Patronymic { get; set; }
+        [Required(ErrorMessage = "Не указана Дата Рождения")]
         public string Birthday { get; set; }
         public bool Writer { get; set; }
     }
