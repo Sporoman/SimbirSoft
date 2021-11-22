0370b8375da42570e435149a59a2805b5e6b72a9
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-22 13:27:25 +0400

    + entity

---
+++ SimbirSoft/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimbirSoft.Models
+{
+    /// <summary>
+    /// 2.2.2 - Класс представления книги
+    /// Часть 2.2; 1 - Добавлена валидация
+    /// </summary>
+    public class Book
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage = "Не указано Название")]
+        public string name { get; set; }
+        [Required(ErrorMessage = "Не указан ID Автора")]
+        public int author_id { get; set; }
+    }
+}
