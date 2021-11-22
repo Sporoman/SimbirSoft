705596989e543ccc95ed52fd96c2d70dde21afb0
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-22 13:29:31 +0400

    + Репозиторий и его интерфейс для книг

---
+++ SimbirSoft/Repositories/BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using SimbirSoft.Models;
+using SimbirSoft.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimbirSoft.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private ContextDb _contextDb;
+
+        public BookRepository(ContextDb context)
+        {
+            _contextDb = context;
+        }
+
+        public List<Book> GetBooks()
+        {
+            return _contextDb.Books.ToList();
+        }
+    }
+}
---
+++ SimbirSoft/Repositories/Interfaces/IBookRepository.cs
@@ -0,0 +1,10 @@
+﻿using SimbirSoft.Models;
+using System.Collections.Generic;
+
+namespace SimbirSoft.Repositories.Interfaces
+{
+    public interface IBookRepository
+    {
+        List<Book> GetBooks();
+    }
+}
