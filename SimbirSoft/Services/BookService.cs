02ae503b4521adfea226f03b84dbf4ef4ac275da
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-22 13:29:56 +0400

    + Сервис под репозиторий книг

---
+++ SimbirSoft/Services/BookService.cs
@@ -0,0 +1,24 @@
+﻿using SimbirSoft.Models;
+using SimbirSoft.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimbirSoft.Services
+{
+    public class BookService
+    {
+        private IBookRepository _repository;
+
+        public BookService(IBookRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Book> GetBooks()
+        {
+            return  _repository.GetBooks();
+        }
+    }
+}
