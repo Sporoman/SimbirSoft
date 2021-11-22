8ca3588a0e43887bba49e7c4507bf0cc2450a06e
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-22 13:30:16 +0400

    + База данных

---
+++ SimbirSoft/ContextDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimbirSoft.Models;
+
+namespace SimbirSoft
+{
+    public class ContextDb : DbContext
+    {
+        public ContextDb(DbContextOptions<ContextDb> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+    }
+}
