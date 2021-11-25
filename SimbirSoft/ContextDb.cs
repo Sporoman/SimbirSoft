f5922b0a5c6b6f99438ab00b752c05b852c7cf29
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-25 09:26:31 +0400

    + added Fluent API

--- SimbirSoft/ContextDb.cs
+++ SimbirSoft/ContextDb.cs
@@ -10,6 +10,14 @@ namespace SimbirSoft
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().ToTable("Book");
+            modelBuilder.Entity<Book>().Property(unit => unit.Id).HasColumnName("id");
+            modelBuilder.Entity<Book>().Property(unit => unit.Name).HasColumnName("name");
+            modelBuilder.Entity<Book>().Property(unit => unit.AuthorId).HasColumnName("author_id");
+        }
+
         public virtual DbSet<Book> Books { get; set; }
     }
 }
