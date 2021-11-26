af9c784f4c4452b4e08c5e2bf7c716b473b3cb53
Dmitriy Nikishin <n1kishin@rambler.ru>
2021-11-26 18:09:07 +0400

    Test

---
+++ SimbirSoft/Scripts/Test/__script_create_db.sql
@@ -0,0 +1,87 @@
+/****** Object: Database [Simbirsoft] ******/
+CREATE DATABASE [simbirSoft]
+GO
+
+USE [simbirSoft]
+GO
+
+/****** Object: Table [authors] ******/
+CREATE TABLE [authors]
+(
+	[id]            [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_authors] PRIMARY KEY,
+	[first_name]    [nvarchar](50) NOT NULL,
+	[last_name]     [nvarchar](50) NOT NULL,
+	[middle_name]   [nvarchar](50) NULL
+)
+GO
+
+/****** Object: Table [persons] ******/
+CREATE TABLE [persons]
+(
+	[id]            [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_persons] PRIMARY KEY,
+	[first_name]    [nvarchar](50) NOT NULL,
+	[last_name]     [nvarchar](50) NOT NULL,
+	[middle_name]   [nvarchar](50) NULL,
+	[birth_date]    [date] NULL
+)
+GO
+
+/****** Object: Table [genres] ******/
+CREATE TABLE [genres]
+(
+	[id]     [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_genres] PRIMARY KEY,
+	[name]   [nvarchar](50) NOT NULL
+)
+GO
+
+/****** Object: Table [books] ******/
+CREATE TABLE [books]
+(
+	[id]          [int] IDENTITY(1,1) NOT NULL CONSTRAINT [PK_books] PRIMARY KEY,
+	[name]        [nvarchar](50) NOT NULL,
+	[author_id]   [int] NOT NULL
+)
+GO
+
+/****** Object: Table [book_genres] ******/
+CREATE TABLE [book_genres]
+(
+	[bookid]    [int] NOT NULL,
+	[genreid]   [int] NOT NULL
+)
+GO
+
+/****** Object: Table [library_cards] ******/
+CREATE TABLE [library_cards]
+(
+	[person_id]     [int] NULL,
+	[book_id]       [int] NULL,
+	[taking_date]   [date] NULL
+)
+GO
+
+/****** Foreign keys ******/
+ALTER TABLE [books] ADD CONSTRAINT [FK_books_authors] 
+	FOREIGN KEY ([author_id]) 
+		REFERENCES [authors] ([id])
+GO
+
+ALTER TABLE [book_genres] ADD CONSTRAINT [FK_book_genres_books] 
+	FOREIGN KEY ([bookid])
+		REFERENCES [books] ([id])
+GO
+
+ALTER TABLE [book_genres] ADD CONSTRAINT [FK_book_genre_genres]
+	FOREIGN KEY ([genreid])
+		REFERENCES [genres] ([id])
+GO
+
+ALTER TABLE [library_cards] ADD CONSTRAINT [FK_library_cards_books]
+	FOREIGN KEY ([book_id])
+		REFERENCES [books] ([id])
+GO
+
+ALTER TABLE [library_cards] ADD CONSTRAINT [FK_library_cards_persons]
+	FOREIGN KEY ([person_id])
+		REFERENCES [persons] ([id])
+GO
---
+++ SimbirSoft/Scripts/Test/__script_insert_data.sql
@@ -0,0 +1,67 @@
+USE [simbirSoft]
+GO
+
+/****** Insert in [persons] ******/
+INSERT [persons] 
+	([first_name], [last_name], [middle_name], [birth_date]) 
+	VALUES 
+	(N'Владимир',   N'Ульянов',    N'Ильич',      CAST(N'1870-04-22' AS Date)),
+	(N'Николай',    N'Воронин',    N'Петрович',   CAST(N'1963-05-12' AS Date)),
+	(N'Андрей',     N'Максимов',   N'Иванович',   CAST(N'2000-10-21' AS Date)),
+	(N'Тестик',     N'Тест',       N'Тестович',   CAST(N'2003-03-23' AS Date))
+GO
+
+/****** Insert in [authors] ******/
+INSERT [authors] 
+	([first_name], [last_name], [middle_name]) 
+	VALUES 
+	(N'Дмитрий',   N'Воронин',      N'Анатольевич'),
+	(N'Гарриет',   N'Бичер-Стоу',   NULL),
+	(N'Кнут',      N'Гамсун',       NULL),
+	(N'Тест',      N'Тестович',     NULL)
+GO
+
+/****** Insert in [genres] ******/
+INSERT [genres] 
+	([name]) 
+	VALUES 
+	(N'Фэнтези'),
+	(N'Роман'),
+	(N'Научная фантастика'),
+	(N'Детектив')
+
+/****** Insert in [books] ******/
+INSERT [books] 
+	([name], [author_id]) 
+	VALUES 
+	(N'Синее пламя', 1),
+	(N'Хижина дяди Тома', 2),
+	(N'Плоды земли', 3),
+	(N'Тестирование', 4),
+	(N'Тестовик', 4),
+	(N'Тест космолёта', 4),
+	(N'Ночной тестировщик', 4)
+GO
+
+/****** Insert in [book_genres] ******/
+INSERT [book_genres] 
+	([bookid], [genreid])
+	VALUES 
+	(1, 1), (2, 2), (3, 2), (4, 1), (5, 2), (6, 3), (7, 4)
+GO
+
+/****** Insert in [library_cards] ******/
+INSERT [library_cards]
+	([person_id], [book_id], [taking_date]) 
+	VALUES 
+	(1, 3, CAST(N'2021-06-12' AS Date)),
+	(2, 3, CAST(N'2021-11-20' AS Date)),
+	(3, 3, CAST(N'2021-11-15' AS Date)),
+	(1, 1, NULL),
+	(2, 2, NULL),
+	(4, 2, CAST(N'2021-10-20' AS Date)),
+	(4, 4, NULL),
+	(4, 5, CAST(N'2021-09-28' AS Date)),
+	(4, 6, CAST(N'2021-11-02' AS Date)),
+	(4, 7, CAST(N'2021-10-29' AS Date))
+GO
